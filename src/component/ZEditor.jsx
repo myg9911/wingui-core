2b0dd1f3ad60d0824a037e8c0cdb4338430848e6
myeonghee <61863525+myg9911@users.noreply.github.com>
2023-01-03 14:40:18 +0900

    modify FileUploader

--- src/component/ZEditor.jsx
+++ src/component/ZEditor.jsx
@@ -9,7 +9,7 @@ import colorSyntax from '@toast-ui/editor-plugin-color-syntax'
 import 'tui-color-picker/dist/tui-color-picker.css'
 import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css'
 import { makeStyles, createStyles } from '@mui/styles';
-import FileUploader from '@zionex/component/FileUploader'
+import FileUploader from './FileUploader'
 
 export const useStyles = makeStyles({
   root: {
